890215ec51d5c9ef8f1a80bba9d98a6b52af257b
VictorGM01 <victormarques8801@gmail.com>
2022-02-05 20:59:01 -0300

    Cria django-forms

---
+++ erros_e_acertos/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from erros_e_acertos.models import Lista, Materia
+
+
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+
+class CadastroForms(forms.ModelForm):
+    data_realizacao = forms.DateField(label='Data da Realização', widget=DateInput())
+
+    class Meta:
+        model = Lista
+        fields = '__all__'
+        labels = {'materia': 'Matéria', 'nome_da_lista': 'Nome da Lista', 'quantidade_questoes': 'Número de Questões',
+                  'tipo': 'Tipo da Lista', 'acertos': 'Quantidade de Acertos', 'erros': 'Quantidade de Erros',
+                  'descricao_erros': 'Descrição do Erro', 'tempo_realizacao': 'Tempo para Realização'}
