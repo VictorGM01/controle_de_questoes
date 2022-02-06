d30a494672f508233ab3df9cd118cae826abe23f
VictorGM01 <victormarques8801@gmail.com>
2022-02-05 21:23:37 -0300

    Cria url, view e template para adição de registros

--- erros_e_acertos/urls.py
+++ erros_e_acertos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, AdicionarRegistro
 
 urlpatterns =[
     path('', index, name='index'),
+    path('add_registro/', AdicionarRegistro.as_view(), name='add-registro')
 ]
--- erros_e_acertos/views.py
+++ erros_e_acertos/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.views.generic import CreateView
+from .forms import CadastroForms
+from .models import Lista
 
 
 def index(request):
     return render(request, 'acertos_e_erros/index.html')
+
+
+class AdicionarRegistro(CreateView):
+    template_name = "acertos_e_erros/adicionar_registros.html"
+    form_class = CadastroForms
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('index')
