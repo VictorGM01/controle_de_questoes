59677c4c0964b58212b4397b62c636e085328d51
VictorGM01 <victormarques8801@gmail.com>
2022-02-05 21:02:13 -0300

    Adiciona django-forms-crispy para futura personalização do form

--- setup/settings.py
+++ setup/settings.py
@@ -26,8 +26,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'erros_e_acertos'
+    'erros_e_acertos',
+    'crispy_forms'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
