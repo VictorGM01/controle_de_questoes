4fe96234ac5b95e8382ece72274b271ebe0057ff
VictorGM01 <victormarques8801@gmail.com>
2022-02-06 00:13:41 -0300

    Adiciona funcionalidade de criar novos registros

--- erros_e_acertos/views.py
+++ erros_e_acertos/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, reverse
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import CreateView
-from .models import Lista
+from .models import Lista, Materia
+from django.contrib.auth.models import User
 
 
 def index(request):
@@ -8,4 +9,26 @@ def index(request):
 
 
 def adicionar_registro(request):
-    return render(request, 'acertos_e_erros/adicionar_registros.html')
+    if request.method == 'POST':
+        nome_lista = request.POST['nome_lista']
+        quantidade_questoes = request.POST['quantidade_questoes']
+        tipo = request.POST['tipo']
+        acertos = request.POST['acertos']
+        erros = request.POST['erros']
+        descricao = request.POST['descricao']
+        time = request.POST['time']
+        data = request.POST['data']
+
+        user = get_object_or_404(User, pk=request.user.id)
+
+        nova_lista = Lista.objects.create(usuario=user, nome_da_lista=nome_lista, quantidade_questoes=quantidade_questoes,
+                                          tipo=tipo, acertos=acertos, erros=erros, descricao_erros=descricao,
+                                          tempo_realizacao=time, data_realizacao=data)
+
+        nova_lista.save()
+
+        return redirect('dashboard')
+
+    contexto = {"materias": sorted(Materia.values)}
+
+    return render(request, 'acertos_e_erros/adicionar_registros.html', contexto)
