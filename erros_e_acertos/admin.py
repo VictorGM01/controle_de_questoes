27bb729fff0d1e33d51396f6be2b8c30bacd3ead
VictorGM01 <victormarques8801@gmail.com>
2022-02-05 21:43:36 -0300

    Registra modelo na página de amdin

--- erros_e_acertos/admin.py
+++ erros_e_acertos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Lista
 
-# Register your models here.
+
+@admin.register(Lista)
+class ListaAdmin(admin.ModelAdmin):
+    list_display = ['materia', 'nome_da_lista', 'quantidade_questoes', 'acertos', 'erros']
