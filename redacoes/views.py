77930a5e1a58a66a2bb295c0e443e28ba6432350
VictorGM01 <victormarques8801@gmail.com>
2022-02-12 13:08:00 -0300

    Cria view para exibição do dashboard de redações

--- redacoes/views.py
+++ redacoes/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+from django.contrib.auth.models import User
+from django.contrib import auth
+from .models import Vestibular, Genero, Redaction
 
 
 def dashboard_redacoes(request):
-    pass
+    if request.user.is_authenticated:
+        id_user = request.user.id
+
+        redacoes = Redaction.objects.order_by('data_realizacao').filter(usuario=id_user)
+
+        generos = sorted(Genero.values)
+
+        vestibulares = sorted(Vestibular.values)
+
+        paginator = Paginator(redacoes, 3)
+
+        pagina = request.GET.get('page')
+
+        redacoes_por_pagina = paginator.get_page(pagina)
+
+        dados = {
+            'redacoes': redacoes_por_pagina,
+            'generos': generos,
+            'vestibulares': vestibulares
+        }
+        return render(request, 'redacoes/dashboard_redacoes.html', dados)
